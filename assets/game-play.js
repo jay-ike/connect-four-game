240968c15c7c592eb6f191a73c7172a3116a4710
Ndimah Mike <ndimah22@protonmail.com>
2023-10-05 23:31:33 +0100

    Feat: implement the game board markup

--- assets/game-play.js
+++ assets/game-play.js
@@ -10,7 +10,10 @@ container.parent.addEventListener("click", function ({target}) {
     if (target.classList.contains("rule-option")) {
         container.gotoStep(1);
     }
-    if (target.classList.contains("rules-read")) {
+    if (target.classList.contains("menu-opt")) {
         container.gotoStep(0);
+    }
+    if (target.classList.contains("game-mode")) {
+        container.gotoStep(2);
     }
 });
