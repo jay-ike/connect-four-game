9bfafb69d426e506e8fc91c48320ec3efb9eba7d
Ndimah Mike <ndimah22@protonmail.com>
2023-10-12 22:53:06 +0100

    Feat: handle the responsive layout of the game mode

--- assets/game-play.js
+++ assets/game-play.js
@@ -1,5 +1,14 @@
 import SteppedForm from "./stepped-form.js";
 
+let board;
+
+function buildPawn(index) {
+    const button = document.createElement("button");
+    button.index = index;
+    button.classList.add("pawn");
+    return button;
+}
+
 const container = new SteppedForm({
     parentClass: "menu-container",
     outClassIndicator: "step-out"
@@ -7,13 +16,24 @@ const container = new SteppedForm({
 container.initialize();
 
 container.parent.addEventListener("click", function ({target}) {
+    let index;
     if (target.classList.contains("rule-option")) {
-        container.gotoStep(1);
+        index = 1;
     }
     if (target.classList.contains("menu-opt")) {
-        container.gotoStep(0);
+        index = 0;
     }
     if (target.classList.contains("game-mode")) {
-        container.gotoStep(2);
+        index = 2;
     }
+    container.gotoStep(index);
+    if (index === 0) {
+        document.body.classList.add("switch-clr");
+    } else {
+        document.body.classList.remove("switch-clr");
+    }
+});
+board = container.parent.querySelector(".game-board");
+Array(42).fill(1).forEach(function (ignore, index) {
+    board.appendChild(buildPawn(index));
 });
